914ecb1f8651471828e224c9f6c24b123e7652a9
Shubam Bhasin <iamshubambhasin@gmail.com>
2021-02-23 20:53:25 +0000

    ex upto page 10

--- index.js
+++ index.js
@@ -23,3 +23,37 @@ console.log(newData)
 birthday(data)
 
 // page 8
+
+// QUESTION
+
+const numArr = [23,12,5,3,4,2,76,45,33,21]
+
+const numLessThanTen = numArr.filter(num => num < 10)
+
+console.log(numLessThanTen) // prints number less than 10
+
+//QUESTION
+
+// taking above numArr
+
+const arrOfObj = numArr.map(num => ({item: num}))
+console.log(arrOfObj)
+
+// QUESTION
+
+const sum = numArr.reduce((a, b) => a+b, 0)
+
+console.log("Total sum = ", sum)
+
+
+// PAGE 11
+
+// QUESTION 
+
+const numberArray = [1,2,3,4,5,6,7,8,9,22,11,32,23,43,37,86,54,31,19]
+
+
+const oddSum =  numberArray.filter(num => num%2 !=0) 
+const evenSum = numberArray.filter(num => num%2==0)
+console.log("oddSum = ",oddSum.reduce((a,b) => a+b, 0))
+console.log("evensum = ",evenSum.reduce((a,b) => a+b, 0))
